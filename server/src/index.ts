59f0d8d3cbbc28c9568bc95b475763ab8fa83d36
ombade <ombade364@gmail.com>
2024-08-28 19:54:36 +0530

    files added

---
+++ server/src/index.ts
@@ -0,0 +1,5 @@
+import Server from 'Server';
+import dotenv from 'dotenv';
+dotenv.config();
+
+new Server().start();
---
+++ vercel-deploy/precache-manifest.f00d8e4565e74af1888346ee6aca6e1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44e960830978e54ab77b3e5e869c18c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d552d95e32c684790fde",
+    "url": "/static/css/main.3dfe1abf.chunk.css"
+  },
+  {
+    "revision": "68d73f842670a1c40ec0",
+    "url": "/static/js/2.7dfff3c4.chunk.js"
+  },
+  {
+    "revision": "f8bf20be83c0843294d3f0aea352d6fc",
+    "url": "/static/js/2.7dfff3c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d552d95e32c684790fde",
+    "url": "/static/js/main.4011f1d1.chunk.js"
+  },
+  {
+    "revision": "77a1c89ed068450af6d0",
+    "url": "/static/js/runtime-main.4635590d.js"
+  }
+]);
